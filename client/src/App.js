bd4a32bd55983ab774e2969a339155bc32e0c208
Shannon Rivers <shannon.rivers@gmail.com>
2017-08-28 14:07:02 -0600

    trying to deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
 import Routing from './components/Router.js';
-import Map from './components/Map.js';
+// import Map from './components/Map.js';
 
 
 class App extends Component {
+  state = {
+    fitbitToken: ''
+  }
+  componentDidMount(){
+    this.checkFitbit();
+  }
+
+  checkFitbit = () => {
+    fetch('/auth/fitbit')
+    .then(res => res.json())
+    .then(fitbitToken => this.setState({fitbitToken}));
+  }
+
   render() {
     return (
       <div className="App">
