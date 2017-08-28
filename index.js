6ced3705048cf3c1fa5ff3e9cf9da5ca580666c6
Shannon Rivers <shannon.rivers@gmail.com>
2017-08-28 11:43:53 -0600

    playing with passport routes

--- index.js
+++ index.js
@@ -49,7 +49,8 @@ passport.deserializeUser(function(obj, done) {
 });
 
 app.get('/auth/fitbit/success', function(req, res, next) {
-  res.send(req.user);
+  // res.send(req.user);
+  res.send('Successful login!')
 });
 
 app.get('/auth/fitbit',
@@ -58,16 +59,16 @@ app.get('/auth/fitbit',
    }
 ));
 
-// app.get('/auth/fitbit/callback', (req,res)=> {
-// })
-
-app.get('/auth/fitbit/callback',
+app.get('/auth/fitbit/callback', (req,res)=> {
   res.send('Fitbit callback reached!');
-  // passport.authenticate('fitbit', {
-  //   successRedirect: '/auth/fitbit/success',
-  //   failureRedirect: '/auth/fitbit/failure'
-  //  }
-));
+})
+
+// app.get('/auth/fitbit/callback',
+//   passport.authenticate('fitbit', {
+//     successRedirect: '/auth/fitbit/success',
+//     failureRedirect: '/auth/fitbit/failure'
+//    }
+// ));
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
