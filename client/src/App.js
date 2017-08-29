cc1c9d353d6cd35d289090fb374795c88c354f0a
Shannon Rivers <shannon.rivers@gmail.com>
2017-08-29 12:33:10 -0600

    add client back in

---
+++ client/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+import Map from './components/Map.js'
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src="https://scontent-dft4-1.cdninstagram.com/t51.2885-19/s320x320/17438844_398287633860003_5443105418219880448_a.jpg" className="App-logo" alt="logo" />
+          <h2 className="App-name">Serene</h2>
+        </div>
+        <Map />
+      </div>
+    );
+  }
+}
+
+export default App;
