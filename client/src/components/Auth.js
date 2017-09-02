8d578485109fedb4e05d90af56c2373548c8f549
Shannon Rivers <shannon.rivers@gmail.com>
2017-09-02 13:40:40 -0600

    fix Auth fetch to https

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -23,7 +23,7 @@ class Auth extends Component{
     async handleSubmit(event) {
     alert('Attempting to login: ' + this.state.username);
     event.preventDefault();
-    const response = await fetch('http://serene-green.herokuapp.com/login', 
+    const response = await fetch('https://serene-green.herokuapp.com/login', 
     // const response = await fetch('http://localhost:5000/login',
     {
       method: 'POST',
