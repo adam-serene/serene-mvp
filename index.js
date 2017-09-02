48155b798dd646431267ba1dddcf54e7e608ae00
Shannon Rivers <shannon.rivers@gmail.com>
2017-09-02 13:45:37 -0600

    remove app.use(express client/build

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'client/build')));
+// app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get('/places', (req, res, next)=>{
   res.send({places: ['place1', 'place2']});
