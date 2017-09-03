c7c86854cfe300a3cc14da1525131f21256edd35
Shannon Rivers <shannon.rivers@gmail.com>
2017-09-03 14:09:15 -0600

    when http/s solved, GoogleMap component will ping server for places and display markers

--- client/src/components/GoogleMap.js
+++ client/src/components/GoogleMap.js
@@ -5,13 +5,31 @@ import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
 export class MapContainer extends React.Component {
   constructor(props) {
     super(props);
-  //   this.state = {
-  //     currentLocation: {
-  //       lat: 40.0150,
-  //       lng: -105.2705
-  //     },
-  //     places: []
-  //   }
+    this.state = {
+      currentLocation: {
+        lat: 40.0150,
+        lng: -105.2705
+      },
+      places: [
+        {id: 1,
+        user_id: 1,
+        description: 'Postcard Point',
+        position: {
+          lat: 39.938945153644035,
+          lng: -105.23653507232666
+        },
+        visits_this_month: 2},
+
+        {id: 2,
+        user_id: 1,
+        description: 'Eldorado Spring Resort & Pool',
+        position: {
+          lat: 39.93183522069995,
+          lng: -105.27945578098297
+        },
+        visits_this_month: 20}
+      ]
+    }
     this.fetchPlaces = this.fetchPlaces.bind(this);
     this.mapClicked = this.mapClicked.bind(this);
     this.centerMoved = this.centerMoved.bind(this);
@@ -36,7 +54,8 @@ export class MapContainer extends React.Component {
     // const {google} = mapProps;
     // const service = new google.maps.places.PlacesService(map);
     console.log('fetchPlaces');
-    const response = await fetch('https://serene-green.herokuapp.com/places');
+    // const response = await fetch('https://serene-green.herokuapp.com/places');
+    const response = await fetch('http://localhost:5000/places');
     const places = await response.json()
       this.setState({
         places: places
@@ -57,26 +76,6 @@ export class MapContainer extends React.Component {
       height: '100vh'
     }
 
-    const places = [
-      {id: 1,
-      user_id: 1,
-      description: 'Postcard Point',
-      position: {
-        lat: 39.938945153644035,
-        lng: -105.23653507232666
-      },
-      visits_this_month: 2},
-
-      {id: 2,
-      user_id: 1,
-      description: 'Eldorado Spring Resort & Pool',
-      position: {
-        lat: 39.93183522069995,
-        lng: -105.27945578098297
-      },
-      visits_this_month: 20}
-    ];
-
     return (
       <div style={style}>
         <Map google={this.props.google}
@@ -84,14 +83,11 @@ export class MapContainer extends React.Component {
           onClick={this.mapClicked}
           onDragend={this.centerMoved}
           style={style}
-          initialCenter={{
-            lat: 40.0150,
-            lng: -105.2705
-          }}
+          initialCenter={this.state.currentLocation}
           zoom={13}
         >
 
-        {places.map(place =>
+        {this.state.places.map(place =>
           <Marker
             key={place.id}
             title={place.description}
