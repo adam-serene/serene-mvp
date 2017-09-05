df5b7c04fcecf4e387bc2872a871e9c594b16e6d
Shannon Rivers <shannon.rivers@gmail.com>
2017-09-05 15:58:52 -0600

    add app.listen back for port

--- index.js
+++ index.js
@@ -170,10 +170,10 @@ app.post('/login', (req,res,next) => {
 //   }
 // });
 
-// const port = process.env.PORT || 5000;
-// app.listen(port);
-//
-// console.log(`Listening on ${port}`);
+const port = process.env.PORT || 5000;
+app.listen(port);
+
+console.log(`Listening on ${port}`);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
