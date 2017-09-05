e8228541cb446a06f201c6fb76c1ff5a83759c2f
Shannon Rivers <shannon.rivers@gmail.com>
2017-09-05 15:49:34 -0600

    remove get / route

--- index.js
+++ index.js
@@ -141,16 +141,16 @@ app.post('/login', (req,res,next) => {
 //   res.sendFile(path.join(__dirname+'/client/build/index.html'));
 // });
 
-app.get('/', (req,res,next)=>{
-  jwt.verify(req.cookies.token, process.env.JWT_KEY, function (err,decoded) {
-    if (err) {
-      res.clearCookie('token');
-      return next(err);
-    }
-    req.user = decoded;
-    res.sendFile(path.join(__dirname+'/client/build/index.html'));
-  });
-});
+// app.get('/', (req,res,next)=>{
+//   jwt.verify(req.cookies.token, process.env.JWT_KEY, function (err,decoded) {
+//     if (err) {
+//       res.clearCookie('token');
+//       return next(err);
+//     }
+//     req.user = decoded;
+//     res.sendFile(path.join(__dirname+'/client/build/index.html'));
+//   });
+// });
 
 // app.use(function (req,res,next) {
 //   if (req.cookies.token) {
