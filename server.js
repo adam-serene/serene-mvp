db7581312e7e8249c6f7d527ab9c153b3db935a5
Adam Smith <asmith13194@gmail.com>
2017-09-18 12:38:40 -0600

    add headers

--- server.js
+++ server.js
@@ -21,6 +21,13 @@ app.use(cors({
   allowedOrigins: ["localhost:*", "serene-green.herokuapp.com", "fitbit.com", "maps.googleapis.com"]
 }));
 
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.use(cookieParser());
 
