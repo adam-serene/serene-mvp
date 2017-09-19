b4955a908aa1b026f79636eeb5032e91d9aab76e
Adam Smith <asmith13194@gmail.com>
2017-09-19 12:30:12 -0600

    cors

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -27,7 +27,7 @@ export default class Auth extends React.Component{
   async handleSubmit(event) {
     event.preventDefault();
     this.notify(`Hey, ${this.state.username}. You down? We'll see....`);
-    const response = await fetch('http://serenegreen.herokuapp.com/login',
+    const response = await fetch('https://serenegreen.herokuapp.com/login',
     {
       method: 'POST',
       headers: {
