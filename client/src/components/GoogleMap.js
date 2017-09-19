b02320b658eb4924e2f9e0755d87a09963b74e8d
Adam Smith <asmith13194@gmail.com>
2017-09-19 14:46:30 -0600

    cors

--- client/src/components/GoogleMap.js
+++ client/src/components/GoogleMap.js
@@ -88,6 +88,11 @@ const theSpots = [
   },
 ]
 
+const myHeaders = new Headers({
+  // "Content-Type": "text/plain",
+  'Access-Control-Allow-Origin': '*'
+})
+
 
 export class MapContainer extends React.Component {
   constructor(props) {
@@ -152,18 +157,18 @@ export class MapContainer extends React.Component {
   async componentWillMount(){
     navigator.geolocation.getCurrentPosition(this.navGCPSuccess, this.navGCPError, navGCPOptions);
     let placesArr = [];
-    // const parkData = await fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?query=park+in+boulder&key=AIzaSyA-c7nBnaF1rAjzLZxQoSN4wWfgiFyTeFs')
-    // const parkDataJson = await parkData.json()
-    // console.log(parkDataJson);
-    axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=park+in+boulder&key=AIzaSyA-c7nBnaF1rAjzLZxQoSN4wWfgiFyTeFs',{headers:{'Access-Control-Allow-Origin': 'https://serenegreen.herokuapp.com/',
-    'Content-Type': 'application/x-www-form-urlencoded',}})
-      .then(({ data }) => {
-        console.log(data);
-        placesArr.push(data)
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    const parkData = await fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?query=park+in+boulder&key=AIzaSyA-c7nBnaF1rAjzLZxQoSN4wWfgiFyTeFs',myHeaders)
+    const parkDataJson = await parkData.json()
+    console.log(parkDataJson);
+    // axios('https://maps.googleapis.com/maps/api/place/textsearch/json?query=park+in+boulder&key=AIzaSyA-c7nBnaF1rAjzLZxQoSN4wWfgiFyTeFs',{headers:{'Access-Control-Allow-Origin': 'https://serenegreen.herokuapp.com',
+    // 'Content-Type': 'application/x-www-form-urlencoded',}})
+    //   .then(({ data }) => {
+    //     console.log(data);
+    //     placesArr.push(data)
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
     // const campgroundData = await fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?query=campground+in+boulder&key=AIzaSyA-c7nBnaF1rAjzLZxQoSN4wWfgiFyTeFs')
     // const campgroundDataJson = await campgroundData.json()
     // const museumData = await fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?query=museum+in+boulder&key=AIzaSyA-c7nBnaF1rAjzLZxQoSN4wWfgiFyTeFs')
