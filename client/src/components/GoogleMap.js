39de6c631b32e7001a99ef123875d9bf36637fdb
Adam Smith <asmith13194@gmail.com>
2017-09-19 17:58:40 -0600

    cors

--- client/src/components/GoogleMap.js
+++ client/src/components/GoogleMap.js
@@ -162,6 +162,7 @@ export class MapContainer extends React.Component {
     const responseJson = await response.json()
     placesArr.push(responseJson.park.results, responseJson.campground.results, responseJson.museum.results, responseJson.amusement.results, theSpots)
     this.setState({places: placesArr});
+    console.log(this.state.placesArr);
   }
 
   render() {
