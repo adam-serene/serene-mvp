d7ea964e8fdf9c74ddddcc4ad1a1fd00a7599061
Adam Smith <asmith13194@gmail.com>
2017-09-19 12:25:46 -0600

    cors

--- client/src/components/GoogleMap.js
+++ client/src/components/GoogleMap.js
@@ -159,7 +159,7 @@ export class MapContainer extends React.Component {
     // const parkData = await fetch('https://maps.googleapis.com/maps/api/place/textsearch/json?query=park+in+boulder&key=AIzaSyA-c7nBnaF1rAjzLZxQoSN4wWfgiFyTeFs')
     // const parkDataJson = await parkData.json()
     // console.log(parkDataJson);
-    axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=park+in+boulder&key=AIzaSyA-c7nBnaF1rAjzLZxQoSN4wWfgiFyTeFs',config)
+    axios.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query=park+in+boulder&key=AIzaSyA-c7nBnaF1rAjzLZxQoSN4wWfgiFyTeFs')
       .then(({ data }) => {
         console.log(data);
       })
